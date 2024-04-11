3a6ca825fa851d4cf2b2cea08baa5d4e95cd8fcb
imbornagainer <kimhooijin@naver.com>
2024-04-11 10:55:58 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240411/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,50 @@
+n, m = map(int, input().split());
+
+x, y = 0, 0
+
+grid = [
+    list(map(int, input().split()))
+    for _ in (range(n))
+]
+
+visited = [
+    [0 for _ in range(n)]
+    for _ in range(m)
+]
+
+Destiny = 0;
+
+def in_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
+
+def CanIgo(x, y):
+    # Grid을 벗어날 경우 x
+    if not in_range(x, y):
+        return False;
+    
+    # 뱀 있을 경우 x
+    if grid[x][y] == 0:
+        return False;
+
+    # 방문한적이 있을 경우 x
+    if visited[x][y] == 1:
+        return False;
+
+    return True;
+
+def DFS(x, y):
+    # dx, dy 좌표설정(아래와 오른쪽으로만 갈 수 있음)
+    # 아래쪽, 오른쪽
+    dxs, dxy = [1, 0],[0, 1];
+    global Destiny;
+
+    for dx, dy in zip(dxs, dxy):
+        new_x, new_y = x + dx, y + dy;
+        if CanIgo(new_x, new_y):
+            visited[new_x][new_y] = 1;
+            Destiny = 1;
+            DFS(new_x, new_y);
+
+Destiny = 0;
+DFS(n-1,m-1);
+print(Destiny)
