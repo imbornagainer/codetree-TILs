f2fdb74f4a364eb8ba9c320ebc0570e97cd9c996
imbornagainer <kimhooijin@naver.com>
2024-04-12 11:12:12 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240412/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240412/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -8,14 +8,14 @@ grid = [
 ]
 
 visited = [
-    [0 for _ in range(m)]
-    for _ in range(n)
+    [0 for _ in range(n)]
+    for _ in range(m)
 ]
 
 q = deque()
 
 def in_range(x,y):
-    return 0 <= x and x < n and 0 <= y and y < n;
+    return 0 <= x and x < n and 0 <= y and y < m;
 
 def CanIgo(x,y):
     # 범위에 벗어나면 False
