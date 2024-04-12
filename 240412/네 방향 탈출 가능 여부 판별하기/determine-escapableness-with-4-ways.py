4ef1aba0ad925bae29031fd2952c6006a3210c4c
imbornagainer <kimhooijin@naver.com>
2024-04-12 11:56:45 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 174ms, Memory: 26MB, Status: Passed - CodeTree

--- 240412/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240412/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-n, m = map(int, input().split());
+n, m = map(int, input().split())
 
 grid = [
     list(map(int, input().split()))
@@ -12,77 +12,60 @@ visited = [
     for _ in range(n)
 ]
 
-q = deque()
+q = deque();
 
-def in_range(x,y):
-    return 0 <= x and x < n and 0 <= y and y < m;
+def in_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < m;
+    # return 0 <= x and x < n and 0 <= y and y < m
 
-def CanIgo(x,y):
-    # 범위에 벗어나면 False
-    if not in_range(x,y):
-        return 0;
+def Can_go(x, y):
+    if not in_range(x, y):
+        return False;
 
-    # 뱀이 있으면 False
-    if (grid[x][y]) == 0:
-        return 0;
+    if grid[x][y] == 0:
+        return False;
     
-    # 가본 곳이면 False
-    if (visited[x][y] == 1):
-        return 0;
-
+    if visited[x][y] == 1:
+        return False;
+    
     return True;
 
-def DSF(x,y):
-    # 상,하,좌,우
-    # dxs, dys = [0,1],[1,0];
-    # dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-    dxs, dys = [0, 0, -1, 1], [-1, 1, 0, 0]
+def BFS():   
 
-    for dx, dy in zip(dxs, dys):
-        new_x, new_y = x + dx, y + dy;
+    while q:
+        x, y = q.popleft();
 
-        if CanIgo(new_x, new_y):
-            visited[new_x][new_y] = 1
-            DSF(new_x,new_y);
+        #상, 하, 좌, 우 (격자는 행, 렬)
+        dxs, dys = [-1,1,0,0],[0,0,-1,1]
 
-visited[0][0];
-DSF(0,0);
+        for dx, dy in zip(dxs, dys):
+            new_x, new_y = x + dx, y + dy;
 
-# for row in visited:
-#     for element in row:
-#         print(element, end=" ")
-#     print();
+            if Can_go(new_x, new_y):
+                visited[new_x][new_y] = 1;
+                q.append((new_x,new_y));
 
-print(visited[n-1][m-1])
-
-# def bfs():
+# def BFS():
 #     # queue에 남은 것이 없을때까지 반복합니다.
 #     while q:
 #         # queue에서 가장 먼저 들어온 원소를 뺍니다.
 #         x, y = q.popleft()
-        
-#         # queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
-#         # dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-#         dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-#         # dxs, dys = [0, 0, -1, 1], [-1, 1, 0, 1]
-#         # dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 
+#         # queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
+#         dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 #         for dx, dy in zip(dxs, dys):
 #             new_x, new_y = x + dx, y + dy
-        
+
 #             # 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
 #             # 새로 queue에 넣어주고 방문 여부를 표시해줍니다.
-#             if CanIgo(new_x, new_y):
+#             if Can_go(new_x, new_y):
 #                 q.append((new_x, new_y))
 #                 visited[new_x][new_y] = 1
 
-                
-# # bfs를 이용해 최소 이동 횟수를 구합니다.
-# # 시작점을 queue에 넣고 시작합니다.
-# q.append((0, 0))
-# visited[0][0] = True
 
-# bfs()
+q.append((0,0));
+visited[0][0] = 1;
 
-# # 우측 하단을 방문한 적이 있는지 여부를 출력합니다.
-# print(visited[-1][-1])
+BFS();
+
+print(visited[-1][-1]);
